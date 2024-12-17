61c28e736919e8ee105967653b6fdd9357e02900
Paul Stearns <paulstearns@Pauls-MacBook-Pro.local>
2024-12-16 19:10:17 -0800

    check apod error

--- next.config.mjs
+++ next.config.mjs
@@ -19,15 +19,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// const nextConfig = {
-//     images: {
-//         domains: [
-//             'apod.nasa.gov',
-//             'images-assets.nasa.gov',
-//             'epic.gsfc.nasa.gov'
-//         ], // Add the NASA API hostname here
-//     },
-// };
-
-// export default nextConfig;
