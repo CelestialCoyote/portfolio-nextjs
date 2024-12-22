d9011a11a4d6747aad00b4b320a6daf220c6ddba
Paul Stearns <paulstearns@Pauls-MacBook-Pro.local>
2024-12-22 15:49:32 -0800

    apod test

---
+++ src/app/api/nasa/apod/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+
+export async function GET() {
+    const date = new Date();
+    const currentDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+
+    try {
+        console.log(`apod current date: ${currentDate}`);
+        const response = await fetch(
+            `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&date=${currentDate}`
+        );
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch data from NASA API");
+        }
+
+        const data = await response.json();
+        console.log(`fetched data: ${data}`)
+        
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
